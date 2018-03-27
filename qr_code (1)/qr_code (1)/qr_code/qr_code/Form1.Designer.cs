61d30d9dd813da1ee979f258c6f9d1dab69c28f7
MOROZOW\Юрий <morozov.yurik@mail.ru>
2018-03-27 17:15:18 +0300

    no message

---
+++ qr_code (1)/qr_code (1)/qr_code/qr_code/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace qr_code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.load_bar = new System.Windows.Forms.Button();
+            this.save_bar = new System.Windows.Forms.Button();
+            this.generate_bar = new System.Windows.Forms.Button();
+            this.scan_bar = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(183, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 182);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(217, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(183, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(22, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Создать QR код";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(331, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить QR код";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(331, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 27);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Загрузить QR код";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(22, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Распознать QR код";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(550, 449);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(542, 423);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "QR-code";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage2.BackgroundImage")));
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.load_bar);
+            this.tabPage2.Controls.Add(this.save_bar);
+            this.tabPage2.Controls.Add(this.generate_bar);
+            this.tabPage2.Controls.Add(this.scan_bar);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(542, 423);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Barcode";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // load_bar
+            // 
+            this.load_bar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load_bar.Location = new System.Drawing.Point(364, 339);
+            this.load_bar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.load_bar.Name = "load_bar";
+            this.load_bar.Size = new System.Drawing.Size(105, 31);
+            this.load_bar.TabIndex = 5;
+            this.load_bar.Text = "Загрузить";
+            this.load_bar.UseVisualStyleBackColor = true;
+            this.load_bar.Click += new System.EventHandler(this.load_bar_Click);
+            // 
+            // save_bar
+            // 
+            this.save_bar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.save_bar.Location = new System.Drawing.Point(364, 293);
+            this.save_bar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.save_bar.Name = "save_bar";
+            this.save_bar.Size = new System.Drawing.Size(105, 31);
+            this.save_bar.TabIndex = 4;
+            this.save_bar.Text = "Сохранить";
+            this.save_bar.UseVisualStyleBackColor = true;
+            this.save_bar.Click += new System.EventHandler(this.save_bar_Click);
+            // 
+            // generate_bar
+            // 
+            this.generate_bar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generate_bar.Location = new System.Drawing.Point(71, 293);
+            this.generate_bar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.generate_bar.Name = "generate_bar";
+            this.generate_bar.Size = new System.Drawing.Size(139, 31);
+            this.generate_bar.TabIndex = 3;
+            this.generate_bar.Text = "Сгенерировать";
+            this.generate_bar.UseVisualStyleBackColor = true;
+            this.generate_bar.Click += new System.EventHandler(this.generate_bar_Click);
+            // 
+            // scan_bar
+            // 
+            this.scan_bar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scan_bar.Location = new System.Drawing.Point(71, 339);
+            this.scan_bar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scan_bar.Name = "scan_bar";
+            this.scan_bar.Size = new System.Drawing.Size(139, 30);
+            this.scan_bar.TabIndex = 2;
+            this.scan_bar.Text = "Сканировать";
+            this.scan_bar.UseVisualStyleBackColor = true;
+            this.scan_bar.Click += new System.EventHandler(this.scan_bar_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(182, 226);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 32);
+            this.textBox2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(130, 48);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(296, 148);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 449);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "QR и Barcode ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button load_bar;
+        private System.Windows.Forms.Button save_bar;
+        private System.Windows.Forms.Button generate_bar;
+        private System.Windows.Forms.Button scan_bar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ qr_code (1)/qr_code (1)/qr_code/qr_code/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using MessagingToolkit.Barcode;
+using System.Text.RegularExpressions;
+
+namespace qr_code
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        BarcodeDecoder Scanner;
+        SaveFileDialog SD;
+        OpenFileDialog OD;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(textBox1.Text, @"\p{IsBasicLatin}")&& textBox1.Text != "") 
+            {
+                string qrtext = textBox1.Text; //считываем текст из TextBox'a
+                QRCodeEncoder encoder = new QRCodeEncoder(); //создаём новую "генерацию кода"
+                Bitmap qrcode = encoder.Encode(qrtext); // кодируем слово, полученное из TextBox'a (qrtext) в переменную qrcode. класса Bitmap(класс, который используется для работы с изображениями)
+                pictureBox1.Image = qrcode as Image; // pictureBox выводит qrcode как изображение.
+            }
+            else
+            {
+                MessageBox.Show("Заполните текстовое поле латинскими буквами");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog save = new SaveFileDialog(); // save будет запрашивать у пользователя место, в которое он захочет сохранить файл. 
+                save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp"; //создаём фильтр, который определяет, в каких форматах мы сможем сохранить нашу информацию. В данном случае выбираем форматы изображений. Записывается так: "название_формата_в обозревателе|*.расширение_формата")
+                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK) //если пользователь нажимает в обозревателе кнопку "Сохранить".
+                {
+                    pictureBox1.Image.Save(save.FileName); //изображение из pictureBox'a сохраняется под именем, которое введёт пользователь
+                }
+                pictureBox1.Image = null;
+                textBox1.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Сгенерируйте штрихкод");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+                OpenFileDialog load = new OpenFileDialog(); //  load будет запрашивать у пользователя место, из которого он хочет загрузить файл.
+                if (load.ShowDialog() == System.Windows.Forms.DialogResult.OK) // //если пользователь нажимает в обозревателе кнопку "Открыть".
+                {
+                    pictureBox1.ImageLocation = load.FileName; // в pictureBox'e открывается файл, который был по пути, запрошенном пользователем.
+                }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                QRCodeDecoder decoder = new QRCodeDecoder(); // создаём "раскодирование изображения"
+                MessageBox.Show(decoder.decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap))); //в MessageBox'e программа запишет раскодированное сообщение с изображения, которое предоврительно будет переведено из pictureBox'a в класс Bitmap, чтобы мы смогли с этим изображением работать. 
+                pictureBox1.Image = null;
+            }
+            catch
+            {
+                MessageBox.Show("Загрузите изображение с QR кодом!");
+                pictureBox1.Image = null;
+            }
+        }
+
+
+
+        private void scan_bar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Scanner = new BarcodeDecoder();
+                Result result = Scanner.Decode(new Bitmap(pictureBox2.Image));
+                MessageBox.Show(result.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Загрузите  ШТРИХКОД");
+            }
+        }
+
+        private void generate_bar_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                pictureBox2.Image = new Bitmap(barcodeEncoder.Encode(BarcodeFormat.Code128, textBox2.Text)); 
+            }
+            catch
+            {
+                  MessageBox.Show("Заполните текстовое поле латинской литерацией");
+            }  
+        }
+
+        private void save_bar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                {
+                    SD = new SaveFileDialog();
+                    SD.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+                    if (SD.ShowDialog() == DialogResult.OK)
+                        pictureBox2.Image.Save(SD.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    pictureBox2.Image = null;
+                    textBox2.Clear();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Сгенерируйте штрихкод");
+            }
+        }
+
+        private void load_bar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OD = new OpenFileDialog();
+                OD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (OD.ShowDialog() == DialogResult.OK)
+                    pictureBox2.Load(OD.FileName);
+            }
+            catch
+            {
+                MessageBox.Show("Выберите файл с расширениями .png, .jpg, .gif, .bmp ");
+            }
+        }
+    }
+}
